965c2a0fec8822e4fd817c6a3698db9f38690ae4
Marc Philipp <mail@marcphilipp.de>
2011-05-27 20:46:00 +0200

    Introduced validation for command-line parsing of port
    Before this change passing only "-i someDir" resulted in the following
    exception:
    
    Exception in thread "main" java.lang.NumberFormatException: null
    	at java.lang.Integer.parseInt(Integer.java:417)
    	at java.lang.Integer.parseInt(Integer.java:499)
    	at jsSlim.JsSlimService.parseCommandLine(JsSlimService.java:28)
    	at jsSlim.JsSlimService.main(JsSlimService.java:11)

--- src/jsSlim/JsSlimServiceTest.java
+++ src/jsSlim/JsSlimServiceTest.java
@@ -1,6 +1,10 @@
 package jsSlim;
 
 import static jsSlim.TestSuite.getTestIncludePath;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 import fitnesse.slim.SlimServiceTestBase;
 
 public class JsSlimServiceTest extends SlimServiceTestBase {
@@ -18,5 +22,24 @@ public class JsSlimServiceTest extends SlimServiceTestBase {
   
   protected String expectedStopTestExceptionMessage() {
     return "ABORT_SLIM_TEST:message:<<test stopped in TestSlim>>";
+  }
+
+  @Test
+  public void validCommandLineWithPort() throws Exception {
+    String[] arguments = {"123"};
+    assertTrue(JsSlimService.parseCommandLine(arguments));
+    assertEquals(123, JsSlimService.port);
+  }
+
+  @Test
+  public void invalidCommandLineWithoutPort() throws Exception {
+    String[] arguments = {"-i", "dir"};
+    assertFalse(JsSlimService.parseCommandLine(arguments));
+  }
+  
+  @Test
+  public void invalidCommandLineWithInvalidPort() throws Exception {
+    String[] arguments = {"-i", "dir", "abc"};
+    assertFalse(JsSlimService.parseCommandLine(arguments));
   }
 }
